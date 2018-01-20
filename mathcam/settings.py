dfb6f884b31213550bfe9c9d2301d4fbe6c58b2b
Akshat Dubey <akshatd4@gmail.com>
2018-01-20 05:47:28 +0530

    Upload v1

--- mathcam/settings.py
+++ mathcam/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '9807434191',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'PORT': '',
     }
 }
 
